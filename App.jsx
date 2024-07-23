e6b075e4bf44ca3700313c236c6d6497d003df16
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2024-07-23 21:18:22 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Contact from "./Component/Contact/Contact";
+import Header from "./Component/Header/Header";
+import Navbar from "./Component/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Contact />
+      <Header />
+    </>
+  );
+}
+
+export default App;
